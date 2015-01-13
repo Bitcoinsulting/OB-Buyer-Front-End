8a403cb122adb69ee8500521e2a80d8ddb462efe
Aeron Paul Sioson <theblacksquid@users.noreply.github.com>
2015-01-13 11:32:02 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
 var app = angular.module('store', ['ngRoute']);
 
-app.config(function($routeProvider) {
+app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
         .when('/', {templateUrl: 'modal/overview.html'})
         .when('/help', {templateUrl: 'modal/help.html'})
         .otherwise({redirectTo: 'modal/404.html'})
-    })
+    }])
